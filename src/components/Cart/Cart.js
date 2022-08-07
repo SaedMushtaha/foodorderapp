70e83cefe63fbdb33376d446a10a07c136819ba8
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-08-07 18:30:25 +0300

    button style and fix the functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,35 @@
-import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
 import { useContext } from 'react';
+
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
-    const cartCtx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
 
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItem= cartCtx.items.length > 0 ;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -24,8 +42,10 @@ const Cart = (props) => {
         <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-       {hasItem &&  <button className={classes.button}>Order</button>}
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
